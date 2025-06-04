147aaf87aa66343697bc04eed8dca8d699fbbca9
Dawit <dawitphotos1@gmail.com>
2025-06-04 09:56:22 -0400

    update

--- routes/enrollmentRoutes.js
+++ routes/enrollmentRoutes.js
@@ -1,13 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const { confirmEnrollment } = require("../controllers/enrollmentController");
-// const authMiddleware = require("../middleware/auth"); // assumes you're protecting the route
-
-// router.post("/confirm", authMiddleware, confirmEnrollment);
-
-// module.exports = router;
-
-
 // routes/enrollmentRoutes.js
 const express = require("express");
 const router = express.Router();
