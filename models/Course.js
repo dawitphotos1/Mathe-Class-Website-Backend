6a3ff73bd4a2d701be0c921e2210668c578ebfd1
Dawit <dawitphotos1@gmail.com>
2025-06-12 15:17:02 -0400

    update

--- models/Course.js
+++ models/Course.js
@@ -1,6 +1,95 @@
 
-// models/Course.js
+// // models/Course.js
+// const { Model, DataTypes } = require("sequelize");
+
+// class Course extends Model {
+//   static associate(models) {
+//     Course.belongsTo(models.User, {
+//       foreignKey: "teacherId",
+//       as: "teacher",
+//     });
+
+//     Course.belongsToMany(models.User, {
+//       through: models.UserCourseAccess,
+//       foreignKey: "courseId",
+//       otherKey: "userId",
+//     });
+
+//     Course.hasMany(models.Lesson, {
+//       foreignKey: "courseId",
+//       as: "lessons",
+//     });
+//   }
+// }
+
+// const initCourse = (sequelize) => {
+//   Course.init(
+//     {
+//       id: {
+//         type: DataTypes.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//         allowNull: false,
+//       },
+//       title: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       slug: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true,
+//       },
+//       description: {
+//         type: DataTypes.TEXT,
+//         allowNull: true,
+//       },
+//       category: {
+//         type: DataTypes.STRING,
+//         allowNull: true,
+//       },
+//       price: {
+//         type: DataTypes.DECIMAL(10, 2),
+//         allowNull: false,
+//         defaultValue: 0.0,
+//       },
+//       teacherId: {
+//         type: DataTypes.INTEGER,
+//         allowNull: true,
+//         references: {
+//           model: "Users",
+//           key: "id",
+//         },
+//         onDelete: "SET NULL",
+//       },
+//     },
+//     {
+//       sequelize,
+//       modelName: "Course",
+//       tableName: "Courses",
+//       timestamps: true,
+//     }
+//   );
+
+//   return Course;
+// };
+
+// module.exports = initCourse;
+
+
+
+
 const { Model, DataTypes } = require("sequelize");
+
+function generateSlug(title) {
+  return title
+    .toLowerCase()
+    .replace(/&/g, "and") // replace & with "and"
+    .replace(/[^a-z0-9\s-]/g, "") // remove non-alphanumeric
+    .replace(/\s+/g, "-") // replace spaces with -
+    .replace(/-+/g, "-") // remove multiple dashes
+    .trim();
+}
 
 class Course extends Model {
   static associate(models) {
@@ -68,6 +157,13 @@ const initCourse = (sequelize) => {
       modelName: "Course",
       tableName: "Courses",
       timestamps: true,
+      hooks: {
+        beforeValidate: (course) => {
+          if (!course.slug && course.title) {
+            course.slug = generateSlug(course.title);
+          }
+        },
+      },
     }
   );
 
