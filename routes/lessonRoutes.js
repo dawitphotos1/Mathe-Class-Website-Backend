ce3ae6866e02d9a04f8aecc0e18d1955dd3d0e80
Dawit <dawitphotos1@gmail.com>
2025-06-21 17:02:29 -0400

    update'

--- routes/lessonRoutes.js
+++ routes/lessonRoutes.js
@@ -1,33 +1,3 @@
-
-// const express = require("express");
-// const router = express.Router();
-// const { Lesson } = require("../models");
-
-// router.get("/courses/:courseId/lessons", async (req, res) => {
-//   try {
-//     const courseId = req.params.courseId;
-//     const lessons = await Lesson.findAll({
-//       where: { courseId },
-//       order: [["orderIndex", "ASC"]],
-//     });
-
-//     if (!lessons.length) {
-//       return res
-//         .status(404)
-//         .json({ success: false, message: "No lessons found." });
-//     }
-
-//     res.json({ success: true, lessons });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ success: false, message: "Server error." });
-//   }
-// });
-
-// module.exports = router;
-
-
-
 const express = require("express");
 const router = express.Router();
 const { Lesson } = require("../models");
