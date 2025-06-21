aa775cdfdd1917c2fa06c13976e2d03e5b10b8e2
Dawit <dawitphotos1@gmail.com>
2025-06-21 17:51:07 -0400

    update

--- config/config.js
+++ config/config.js
@@ -60,23 +60,26 @@
 //   },
 // };
 
-
 require("dotenv").config();
+
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   development: {
     username: "mathclass",
     password: "fullpIXDDyw9PwQB6mOqczkt4AYIWhNO",
     database: "mathclassdb",
-    host: "dpg-d0n5a095pdvs738a7isg-a.oregon-postgres.render.com",
+    host: "localhost", // use localhost for local dev (if you have a local DB)
     dialect: "postgres",
     logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    dialectOptions: isProduction
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
     pool: {
       max: 10,
       min: 0,
@@ -85,7 +88,6 @@ module.exports = {
     },
   },
   test: {
-    // for local tests if needed
     username: "mathclass",
     password: "fullpIXDDyw9PwQB6mOqczkt4AYIWhNO",
     database: "mathclassdb_test",
