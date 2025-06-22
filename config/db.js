9075f374769c8701a40f0373bfd994a57ec7da82
Dawit <dawitphotos1@gmail.com>
2025-06-21 20:42:58 -0400

    update

--- config/db.js
+++ config/db.js
@@ -1,38 +1,4 @@
 
-// // config/db.js
-// require("dotenv").config();
-// const { Sequelize } = require("sequelize");
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// });
-
-// sequelize
-//   .authenticate()
-//   .then(() => console.log("✅ PostgreSQL connection established"))
-//   .catch((err) => {
-//     console.error("❌ Database connection failed:", err);
-//     process.exit(1);
-//   });
-
-// module.exports = sequelize;
-
-// config/db.js
-
-
-
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const config = require("./config.js");
