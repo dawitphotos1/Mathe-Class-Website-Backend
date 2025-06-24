c125c3651842b574d2cb80bcea409a9c91274dbf
Dawit <dawitphotos1@gmail.com>
2025-06-24 08:26:21 -0400

    update

--- migrations/20250623124932-add-fields-to-courses.js
+++ migrations/20250623124932-add-fields-to-courses.js
@@ -1,45 +1,3 @@
-// "use strict";
-
-// module.exports = {
-//   up: async (queryInterface, DataTypes) => {
-//     const tableInfo = await queryInterface.describeTable("Courses");
-//     if (!tableInfo.studentCount) {
-//       await queryInterface.addColumn("Courses", "studentCount", {
-//         type: DataTypes.INTEGER,
-//         allowNull: true,
-//         defaultValue: 0,
-//       });
-//     }
-//     if (!tableInfo.thumbnail) {
-//       await queryInterface.addColumn("Courses", "thumbnail", {
-//         type: DataTypes.STRING,
-//         allowNull: true,
-//       });
-//     }
-//     if (!tableInfo.introVideoUrl) {
-//       await queryInterface.addColumn("Courses", "introVideoUrl", {
-//         type: DataTypes.STRING,
-//         allowNull: true,
-//       });
-//     }
-//   },
-
-//   down: async (queryInterface) => {
-//     const tableInfo = await queryInterface.describeTable("Courses");
-//     if (tableInfo.studentCount) {
-//       await queryInterface.removeColumn("Courses", "studentCount");
-//     }
-//     if (tableInfo.thumbnail) {
-//       await queryInterface.removeColumn("Courses", "thumbnail");
-//     }
-//     if (tableInfo.introVideoUrl) {
-//       await queryInterface.removeColumn("Courses", "introVideoUrl");
-//     }
-//   },
-// };
-
-
-
 "use strict";
 
 module.exports = {
@@ -66,7 +24,7 @@ module.exports = {
     }
   },
 
-  down: async (queryInterface) {
+  down: async (queryInterface) => {
     const tableInfo = await queryInterface.describeTable("Courses");
     if (tableInfo.studentCount) {
       await queryInterface.removeColumn("Courses", "studentCount");
