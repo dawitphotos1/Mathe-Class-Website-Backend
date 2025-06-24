1311770ce2162cd7eb15dce8f985a00f32fbda01
Dawit <dawitphotos1@gmail.com>
2025-06-24 10:42:02 -0400

    update

--- migrations/20250517000000-add-approved-to-user-course-access.js
+++ migrations/20250517000000-add-approved-to-user-course-access.js
@@ -1,20 +1,3 @@
-// "use strict";
-
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
-//     await queryInterface.addColumn("UserCourseAccess", "approved", {
-//       type: Sequelize.BOOLEAN,
-//       defaultValue: false,
-//     });
-//   },
-
-//   async down(queryInterface, Sequelize) {
-//     await queryInterface.removeColumn("UserCourseAccess", "approved");
-//   },
-// };
-
-
-
 
 "use strict";
 
