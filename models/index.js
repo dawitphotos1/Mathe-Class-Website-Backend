be8f2a23643d3600a65e385d6cd51e8a05d75629
Dawit <dawitphotos1@gmail.com>
2025-06-25 11:08:20 -0400

    update

--- models/index.js
+++ models/index.js
@@ -1,30 +1,32 @@
 
-// models/index.js
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
+// Model initializers
 const initUser = require("./User");
 const initLesson = require("./Lesson");
 const initCourse = require("./Course");
 const initUserCourseAccess = require("./UserCourseAccess");
+const initLessonCompletion = require("./lessoncompletion"); // ← Add this
 
 const models = {};
-LessonCompletion: require("./lessoncompletion")(sequelize, DataTypes),
-  // Sequelize setup
-  (models.Sequelize = Sequelize);
-models.sequelize = sequelize;
 
 // Initialize models
 models.User = initUser(sequelize);
 models.Lesson = initLesson(sequelize);
 models.Course = initCourse(sequelize);
 models.UserCourseAccess = initUserCourseAccess(sequelize);
+models.LessonCompletion = initLessonCompletion(sequelize, DataTypes); // ← Corrected assignment
+
+// Sequelize setup
+models.Sequelize = Sequelize;
+models.sequelize = sequelize;
 
 // 🔐 Setup associations (after models are initialized)
 if (models.User.associate) models.User.associate(models);
 if (models.Lesson.associate) models.Lesson.associate(models);
 if (models.Course.associate) models.Course.associate(models);
 if (models.UserCourseAccess.associate) models.UserCourseAccess.associate(models);
+if (models.LessonCompletion.associate) models.LessonCompletion.associate(models); // Optional
 
 module.exports = models;
-
--- models/lessoncompletion.js
+++ models/lessoncompletion.js
@@ -1,10 +1,23 @@
+// models/lessoncompletion.js
+
 module.exports = (sequelize, DataTypes) => {
   const LessonCompletion = sequelize.define("LessonCompletion", {
-    userId: { type: DataTypes.INTEGER, allowNull: false },
-    lessonId: { type: DataTypes.INTEGER, allowNull: false },
-    completedAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    lessonId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    completedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
   });
 
+  // Optional: associations
   LessonCompletion.associate = (models) => {
     LessonCompletion.belongsTo(models.User, { foreignKey: "userId" });
     LessonCompletion.belongsTo(models.Lesson, { foreignKey: "lessonId" });
