cbd8ebb274b08f14bbcb2cfca42b1e0e51f587e5
Dawit <dawitphotos1@gmail.com>
2025-06-26 12:51:43 -0400

    update

--- models/index.js
+++ models/index.js
@@ -1,4 +1,44 @@
 
+// const { Sequelize, DataTypes } = require("sequelize");
+// const sequelize = require("../config/db");
+
+// // Import model definitions
+// const initUser = require("./User");
+// const initLesson = require("./Lesson");
+// const initCourse = require("./Course");
+// const initUserCourseAccess = require("./UserCourseAccess");
+// const initLessonCompletion = require("./lessoncompletion");
+
+// const models = {};
+
+// // Initialize models
+// models.User = initUser(sequelize, DataTypes);
+// models.Lesson = initLesson(sequelize, DataTypes);
+// models.Course = initCourse(sequelize, DataTypes);
+// models.UserCourseAccess = initUserCourseAccess(sequelize, DataTypes);
+// models.LessonCompletion = initLessonCompletion(sequelize, DataTypes);
+
+// // Apply associations
+// Object.values(models).forEach((model) => {
+//   if (typeof model.associate === "function") {
+//     model.associate(models);
+//   }
+// });
+
+// Object.keys(db).forEach((modelName) => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+
+// // Export Sequelize instance and models
+// models.sequelize = sequelize;
+// models.Sequelize = Sequelize;
+
+// module.exports = models;
+
+
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
@@ -25,16 +65,8 @@ Object.values(models).forEach((model) => {
   }
 });
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-
-// Export Sequelize instance and models
+// Attach Sequelize instance and library
 models.sequelize = sequelize;
 models.Sequelize = Sequelize;
 
 module.exports = models;
-
