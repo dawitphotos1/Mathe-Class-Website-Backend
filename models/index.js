187ce5a1abb6899299bad5c786d60c42ab3ff4ff
Dawit <dawitphotos1@gmail.com>
2025-06-26 19:53:22 -0400

    update

--- models/index.js
+++ models/index.js
@@ -1,3 +1,37 @@
+// const { Sequelize, DataTypes } = require("sequelize");
+// const sequelize = require("../config/db");
+
+// // Import model definitions
+// const initUser = require("./User");
+// const initLesson = require("./Lesson");
+// const initCourse = require("./Course");
+// const initUserCourseAccess = require("./UserCourseAccess");
+// const initLessonCompletion = require("./lessoncompletion");
+
+// const models = {};
+
+// // Initialize models
+// models.User = initUser(sequelize, DataTypes);
+// models.Lesson = initLesson(sequelize, DataTypes);
+// models.Course = initCourse(sequelize, DataTypes);
+// models.UserCourseAccess = initUserCourseAccess(sequelize, DataTypes);
+// models.LessonCompletion = initLessonCompletion(sequelize, DataTypes);
+
+// // Apply associations
+// Object.values(models).forEach((model) => {
+//   if (typeof model.associate === "function") {
+//     model.associate(models);
+//   }
+// });
+
+// // Attach Sequelize instance and library
+// models.sequelize = sequelize;
+// models.Sequelize = Sequelize;
+
+// module.exports = models;
+
+
+
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
@@ -7,6 +41,7 @@ const initLesson = require("./Lesson");
 const initCourse = require("./Course");
 const initUserCourseAccess = require("./UserCourseAccess");
 const initLessonCompletion = require("./lessoncompletion");
+const initLessonProgress = require("./lessonProgress"); // ✅ Add this line
 
 const models = {};
 
@@ -16,6 +51,7 @@ models.Lesson = initLesson(sequelize, DataTypes);
 models.Course = initCourse(sequelize, DataTypes);
 models.UserCourseAccess = initUserCourseAccess(sequelize, DataTypes);
 models.LessonCompletion = initLessonCompletion(sequelize, DataTypes);
+models.LessonProgress = initLessonProgress(sequelize, DataTypes); // ✅ Register model
 
 // Apply associations
 Object.values(models).forEach((model) => {
---
+++ models/lessonProgress.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const LessonProgress = sequelize.define("LessonProgress", {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    lessonId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  });
+
+  return LessonProgress;
+};
