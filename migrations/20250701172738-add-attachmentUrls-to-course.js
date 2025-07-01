3e60ad8eb3d760cca2fdeb4603f13a3c6a28bd22
Dawit <dawitphotos1@gmail.com>
2025-07-01 13:44:36 -0400

    update

--- migrations/20250701172738-add-attachmentUrls-to-course.js
+++ migrations/20250701172738-add-attachmentUrls-to-course.js
@@ -1,15 +1,36 @@
+// "use strict";
+
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.addColumn("Courses", "attachmentUrls", {
+//       type: Sequelize.ARRAY(Sequelize.STRING), // match the model definition
+//       allowNull: true,
+//       defaultValue: [],
+//     });
+//   },
+
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.removeColumn("Courses", "attachmentUrls");
+//   },
+// };
+
+
+
 "use strict";
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Courses", "attachmentUrls", {
-      type: Sequelize.ARRAY(Sequelize.STRING), // match the model definition
-      allowNull: true,
-      defaultValue: [],
-    });
+  up: async (queryInterface, Sequelize) => {
+    const tableDescription = await queryInterface.describeTable("Courses");
+
+    if (!tableDescription.attachmentUrls) {
+      await queryInterface.addColumn("Courses", "attachmentUrls", {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        allowNull: true,
+      });
+    }
   },
 
-  async down(queryInterface, Sequelize) {
+  down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn("Courses", "attachmentUrls");
   },
 };
