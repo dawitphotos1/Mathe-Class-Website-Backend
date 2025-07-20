070481983414439a32a2ef6d4818798e0d283087
Dawit <dawitphotos1@gmail.com>
2025-07-20 12:19:10 -0400

    Add table creation and test course in server.js

--- utils/axios.js
+++
@@ -1,9 +0,0 @@
-// src/utils/axios.js
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:5000/api/v1",
-  withCredentials: true,
-});
-
-export default instance;
