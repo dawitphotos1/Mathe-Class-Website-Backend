64c32e9b26bf76cd9e38c9aae3f48a8ec782cb78
Dawit <dawitphotos1@gmail.com>
2025-08-03 16:00:57 -0400

    Add table creation and test course in server.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,35 +1,3 @@
-
-// const jwt = require("jsonwebtoken");
-// const { User } = require("../models");
-
-// module.exports = async (req, res, next) => {
-//   const authHeader = req.headers.authorization;
-
-//   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//     return res.status(401).json({ error: "Unauthorized - No token provided" });
-//   }
-
-//   const token = authHeader.split(" ")[1];
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//     const user = await User.findByPk(decoded.id);
-
-//     if (!user) {
-//       return res.status(401).json({ error: "Unauthorized - User not found" });
-//     }
-
-//     req.user = user; // ✅ Attach user to request
-//     next();
-//   } catch (err) {
-//     console.error("JWT verification failed:", err.message);
-//     return res.status(401).json({ error: "Unauthorized - Invalid token" });
-//   }
-// };
-
-
-
 // Example /middleware/auth.js
 
 const jwt = require("jsonwebtoken");
--- middleware/checkTeacherOrAdmin.js
+++ middleware/checkTeacherOrAdmin.js
@@ -1,11 +1,3 @@
-// module.exports = function (req, res, next) {
-//   const user = req.user;
-//   if (!user || (user.role !== "teacher" && user.role !== "admin")) {
-//     return res.status(403).json({ success: false, error: "Access denied" });
-//   }
-//   next();
-// };
-
 
 
 module.exports = (req, res, next) => {
