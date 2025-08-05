19fad0e62c116c6d26e3acfd63da3df70c6be54e
Dawit <dawitphotos1@gmail.com>
2025-08-05 09:35:32 -0400

    Add table creation and test course in server.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,35 +1,3 @@
-
-// const errorHandler = (err, req, res, next) => {
-//   console.error(`[${new Date().toISOString()}] Error:`, {
-//     message: err.message,
-//     stack: err.stack,
-//     path: req.path,
-//     method: req.method,
-//     body: req.body,
-//     headers: req.headers,
-//     userId: req.user?.id,
-//   });
-
-//   const statusCode = err.statusCode || 500;
-//   const message = statusCode === 500 ? "Internal server error" : err.message;
-
-//   res.status(statusCode).json({
-//     success: false,
-//     message,
-//     ...(process.env.NODE_ENV === "development" && {
-//       stack: err.stack,
-//       error: err.message,
-//       requestBody: req.body,
-//       requestHeaders: req.headers,
-//     }),
-//   });
-// };
-
-// module.exports = errorHandler;
-
-
-
-
 module.exports = (err, req, res, next) => {
   console.error(`[${new Date().toISOString()}] Error:`, {
     message: err.message,
--- middleware/validateRequest.js
+++ middleware/validateRequest.js
@@ -1,26 +1,3 @@
-// const Joi = require("joi");
-
-// // Middleware factory for validating request body
-// const validateRequest = (schema) => {
-//   return (req, res, next) => {
-//     const { error } = schema.validate(req.body, { abortEarly: false });
-//     if (error) {
-//       return res.status(400).json({
-//         success: false,
-//         error: "Validation error",
-//         details: error.details.map((d) => d.message),
-//       });
-//     }
-//     next();
-//   };
-// };
-
-// module.exports = validateRequest;
-
-
-
-
-
 
 const Joi = require("joi");
 
