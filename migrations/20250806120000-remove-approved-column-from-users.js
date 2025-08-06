dd33d8746fa063089d663a213c8616fdbe883887
Dawit <dawitphotos1@gmail.com>
2025-08-06 12:21:00 -0400

    Add table creation and test course in server.js

---
+++ migrations/20250806120000-remove-approved-column-from-users.js
@@ -0,0 +1,46 @@
+// "use strict";
+
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     const tableInfo = await queryInterface.describeTable("Users");
+
+//     if (tableInfo.approved) {
+//       console.log("🧹 Removing 'approved' column from Users table...");
+//       await queryInterface.removeColumn("Users", "approved");
+//     }
+//   },
+
+//   down: async (queryInterface, Sequelize) => {
+//     console.log("🔁 Re-adding 'approved' column to Users table...");
+//     await queryInterface.addColumn("Users", "approved", {
+//       type: Sequelize.BOOLEAN,
+//       defaultValue: false,
+//       allowNull: false,
+//     });
+//   },
+// };
+
+
+
+
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const tableInfo = await queryInterface.describeTable("Users");
+
+    if (tableInfo.approved) {
+      console.log("🧹 Removing 'approved' column from Users table...");
+      await queryInterface.removeColumn("Users", "approved");
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    console.log("🔁 Re-adding 'approved' column to Users table...");
+    await queryInterface.addColumn("Users", "approved", {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    });
+  },
+};
