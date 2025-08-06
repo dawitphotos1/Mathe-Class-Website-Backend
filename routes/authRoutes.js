dbb963b2a3a2954c669f7bd7f1ed2a5a0297c70b
Dawit <dawitphotos1@gmail.com>
2025-08-05 21:50:27 -0400

    Add table creation and test course in server.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// // Import the full controller object
-// const authController = require("../controllers/authController");
-
-// // Use methods from the controller
-// router.post("/register", authController.register);
-// router.post("/login", authController.login);
-
-// module.exports = router;
-
 
 // routes/authRoutes.js
 const express = require("express");
--- routes/enrollmentRoutes.js
+++ routes/enrollmentRoutes.js
@@ -1,21 +1,3 @@
-// // routes/enrollmentRoutes.js
-// const express = require("express");
-// const router = express.Router();
-// const auth = require("../middleware/authenticateToken");
-// const {
-//   confirmEnrollment,
-//   checkEnrollmentStatus,
-// } = require("../controllers/enrollmentController");
-
-// // Confirm enrollment (student triggers this)
-// router.post("/confirm", auth, confirmEnrollment);
-
-// // Check if student is enrolled in a course (used to decide if 'Enroll Now' should be shown)
-// router.get("/check/:courseId", auth, checkEnrollmentStatus);
-
-// module.exports = router;
-
-// routes/enrollmentRoutes.js
 
 const express = require("express");
 const router = express.Router();
