c50ea81ba001d0431e8a30ec31377ce9e9ffc062
Dawit <dawitphotos1@gmail.com>
2025-08-12 10:38:43 -0400

    Add table creation and test course in server.js

--- config/db.js
+++ config/db.js
@@ -1,39 +1,3 @@
-
-// require("dotenv").config();
-// const { Sequelize } = require("sequelize");
-// const config = require("./config");
-
-// const env = process.env.NODE_ENV || "development";
-// const dbConfig = config[env];
-
-// let sequelize;
-
-// if (dbConfig.use_env_variable && process.env[dbConfig.use_env_variable]) {
-//   sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
-// } else if (dbConfig.url) {
-//   sequelize = new Sequelize(dbConfig.url, dbConfig);
-// } else {
-//   sequelize = new Sequelize(
-//     dbConfig.database,
-//     dbConfig.username,
-//     dbConfig.password,
-//     dbConfig
-//   );
-// }
-
-// // Optional: test DB connection
-// sequelize
-//   .authenticate()
-//   .then(() => console.log("✅ PostgreSQL connection established"))
-//   .catch((err) => {
-//     console.error("❌ Database connection failed:", err);
-//     process.exit(1);
-//   });
-
-// module.exports = sequelize;
-
-
-
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
