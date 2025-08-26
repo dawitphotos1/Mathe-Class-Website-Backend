5110f86ec582d7d2fdc67e7fdbbbe05266496ff2
Dawit <dawitphotos1@gmail.com>
2025-08-26 18:07:20 -0400

    Add table creation and test course in server.js

---
+++ migrations/20250825120000-add-email-notification-status-to-users.js
@@ -0,0 +1,31 @@
+// "use strict";
+
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.addColumn("users", "email_notification_status", {
+//       type: Sequelize.ENUM("sent", "failed"),
+//       defaultValue: "failed",
+//     });
+//   },
+
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.removeColumn("users", "email_notification_status");
+//   },
+// };
+
+
+
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("users", "email_notification_status", {
+      type: Sequelize.ENUM("sent", "failed"),
+      defaultValue: "failed",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("users", "email_notification_status");
+  },
+};
